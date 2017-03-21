1e2bb56ff20d1aeb561d1f538af1e14558bc574a
yangchen8710 <chen.yang8710@hotmail.com>
2017-03-21 16:08:23 +0800

    tabletennis base class

---
+++ chap13/usett0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "tabtenn0.h"
+int main()
+{
+	using std::cout;
+	TableTennisPlayer player1("Chuck", "Blizzard", true);
+	TableTennisPlayer player2("Tara",  "Boomea", false);
+	player1.Name();
+	if (player1.HasTable())
+		cout << ": has a table.\n";
+	else
+		cout << ": hasn't a table.\n";
+	player2.Name();
+	if (player2.HasTable())
+		cout << " : has a table.\n";
+	else
+		cout << " :hasn't a table.\n";
+
+	return 0;
+}
